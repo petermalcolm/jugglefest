8c06fe93e9d4fd6e4895adb145e650b2ad874545
petermalcolm <peter.t.malcolm@gmail.com>
2017-12-26 14:29:25 -0500

    subtract_col_minima, private methods on hung.

--- hungarian.rb
+++ hungarian.rb
@@ -21,79 +21,89 @@ class Hungarian
 		# do the algorithm:
 		@global_max = invert
 		subtract_row_minima
+		subtract_column_minima
+		evaluate
 
 		puts "Grid \n" + @grid.inspect
-		puts "Copy \n" + copy.inspect
+		# puts "Copy \n" + copy.inspect
 	end
 
-	def ingest_file(file_name)
-		grid = Array.new
-		begin
-			File.open(file_name).each do |line|
-				grid.push(line.split)
-			end
-		rescue Exception => msg
-			# display the system generated error message
-			puts msg  
-		end	
-		return grid	
-	end
+	protected
 
-	def grid_clone # deep clone
-		copy = Array.new
-		@grid.each do |row|
-			if row.count != @grid.count # sanity check
-				puts "Error. Input data is not square"
-				exit
+		def ingest_file(file_name)
+			grid = Array.new
+			begin
+				File.open(file_name).each do |line|
+					grid.push(line.split)
+				end
+			rescue Exception => msg
+				# display the system generated error message
+				puts msg
 			end
-			temp_row = Array.new
-			row.each do |val|
-				temp_row.push(val.to_i)
-			end
-			copy.push(temp_row)
+			return grid
 		end
-		return copy
-	end
 
-	# invert the sign of the grid, then add max
-	def invert
-		max = 0
-		@grid.each_with_index do |row, r_idx|
-			row.each_with_index do |val, v_idx|
-				if val.to_i > max then max = val.to_i end
-				@grid[r_idx][v_idx] = -val.to_i
+		def grid_clone # deep clone
+			copy = Array.new
+			@grid.each do |row|
+				if row.count != @grid.count # sanity check
+					puts "Error. Input data is not square"
+					exit
+				end
+				temp_row = Array.new
+				row.each do |val|
+					temp_row.push(val.to_i)
+				end
+				copy.push(temp_row)
+			end
+			return copy
+		end
+
+		# invert the sign of the grid, then add max
+		def invert
+			max = 0
+			@grid.each_with_index do |row, r_idx|
+				row.each_with_index do |val, v_idx|
+					if val.to_i > max then max = val.to_i end
+					@grid[r_idx][v_idx] = -val.to_i
+				end
+			end
+			@grid.each_with_index do |row, r_idx|
+				row.each_with_index do |val, v_idx|
+					@grid[r_idx][v_idx] = val + max
+				end
+			end
+			return max
+		end
+
+		def subtract_row_minima
+			@grid.each_with_index do |row, r_idx|
+				min = @global_max
+				row.each do |val|
+					if val < min then min = val end
+				end
+				row.each_with_index do |val, v_idx|
+					@grid[r_idx][v_idx] = val - min
+				end
 			end
 		end
-		@grid.each_with_index do |row, r_idx|
-			row.each_with_index do |val, v_idx|
-				@grid[r_idx][v_idx] = val + max
+
+		def subtract_column_minima
+			@grid[0].each_with_index do |val, v_idx|
+				min = @global_max
+				@grid.each do |row|
+					if row[v_idx] < min then min = row[v_idx] end
+				end
+				@grid.each_with_index do |row, r_idx|
+					@grid[r_idx][v_idx] = row[v_idx] - min
+				end
 			end
 		end
-		return max
-	end
 
-	def subtract_row_minima
-		@grid.each_with_index do |row, r_idx|
-			min = @global_max
-			row.each do |val|
-				if val < min then min = val end
-			end
-			row.each_with_index do |val, v_idx|
-				@grid[r_idx][v_idx] = val - min
-			end
+		def evaluate
 		end
-	end
 
-	def subtract_column_minima
-		@grid[0].each_with_index do |val, v_idx|
-
+		def break_ties
 		end
-	end
-
-	def evaluate
-	end
-
-	def break_ties
-	end
 
 end
