31c8f5e411dd04e27dc2e3675a719525dfa809b8
petermalcolm <peter.t.malcolm@gmail.com>
2017-12-26 10:42:28 -0500

    parse input file

--- main.rb
+++ main.rb
@@ -6,18 +6,48 @@ class JuggleFest
 		  puts "Too few arguments"
 		  exit
 		end
+		@circuits = Hash.new
+		@jugglers = Hash.new
 		ingest_file(ARGV[0])
+		puts "Circuits: \n" + @circuits.inspect
+		puts "Jugglers: \n" + @jugglers.inspect
 	end
 
 	def ingest_file(file_name)
 		begin
 			File.open(file_name).each do |line|
-				puts line
+				parse_line(line)
 			end
 		rescue Exception => msg
 			# display the system generated error message
 			puts msg  
 		end		
+	end
+
+	def parse_line(line)
+		line = line.split
+		if line.empty? or ( line[0] != 'C' and line[0] != 'J' )
+			return
+		end
+		if 'C' === line[0]
+			temp = Hash.new
+			for idx in 2..(line.count - 1)
+				pair = line[idx].split(':')
+				temp[pair[0]] = pair[1]
+			end
+			@circuits[line[1]] = temp
+		elsif 'J' === line[0]
+			temp = Hash.new
+			for idx in 2..(line.count - 1)
+				if line[idx].include? ":"
+					pair = line[idx].split(':')
+					temp[pair[0]] = pair[1]
+				else # must be preferences
+					temp['prefs'] = line[idx].split(',')
+				end
+			end
+			@jugglers[line[1]] = temp
+		end
 	end
 
 
