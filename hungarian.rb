1c1c69650b8fcc1752b0ab13bfe61f1115dbd8e1
petermalcolm <peter.t.malcolm@gmail.com>
2017-12-27 16:46:36 -0500

    hungarian: assign and mark

--- hungarian.rb
+++ hungarian.rb
@@ -2,7 +2,7 @@
 # Big thanks to http://www.hungarianalgorithm.com/ for step-by-step
 
 class Hungarian
-	attr_reader :solved
+	attr_reader :solved, :assignments
 
 	# grid must be a square array
 	def initialize(grid, tie_breaker_grid, file_name)
@@ -25,7 +25,13 @@ class Hungarian
 		@global_max = invert
 		subtract_row_minima
 		subtract_column_minima
-		evaluate
+		@assignments = assign
+		if @assignments[ :row ].count === @grid.count
+			@solved = true
+		else
+			marked = mark
+			puts marked.inspect
+		end
 
 		puts "Grid \n" + @grid.inspect
 		# puts "Copy \n" + copy.inspect
@@ -101,6 +107,57 @@ class Hungarian
 					@grid[r_idx][v_idx] = row[v_idx] - min
 				end
 			end
+		end
+
+		def assign
+			r_assignments = Array.new # rows
+			c_assignments = Array.new # columns
+			@grid.each_with_index do |row, r_idx|
+				row.each_with_index do |val, v_idx|
+					if 0 === val
+						if !r_assignments.include? r_idx and !c_assignments.include? v_idx
+							r_assignments.push(r_idx)
+							c_assignments.push(v_idx)
+						end
+					end
+				end
+			end
+			return { :row => r_assignments, :column => c_assignments }
+		end
+
+		def mark
+			unassigned_or_duplicate_rows = Array.new
+			capturing_columns = Array.new
+			capturing_rows = Array (0..@grid.count - 1) # invert u_or_d later
+			(0..@grid.count - 1).each do |u_row| # visit all rows
+				if @assignments[ :row ].include? u_row # skip if they are assigned
+					next
+				end
+				if !unassigned_or_duplicate_rows.include? u_row
+					unassigned_or_duplicate_rows.push(u_row)
+				end
+				@grid[u_row].each_with_index do |val, v_idx|
+					if 0 === val and !capturing_columns.include? v_idx
+						capturing_columns.push(v_idx) # capturing cols get zeroes from un_rows
+						@grid.each_with_index do |row, r_idx|
+							if assigned?(r_idx,v_idx) and !unassigned_or_duplicate_rows.include? r_idx
+								unassigned_or_duplicate_rows.push(r_idx)
+							end
+						end
+					end
+				end
+			end
+			capturing_rows = capturing_rows - unassigned_or_duplicate_rows
+			return { :row => capturing_rows, :column => capturing_columns }
+		end
+
+		def assigned?(r,c)
+			if @assignments[ :row ].include? r and @assignments[ :column ].include? c
+				if @assignments[ :row ].index(r) == @assignments[ :column ].index(c)
+					return true
+				end
+			end
+			return false
 		end
 
 		def evaluate
