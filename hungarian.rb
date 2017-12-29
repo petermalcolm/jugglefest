36eb6c77887eb45ff3f4efeaa44f5b649ee3d625
petermalcolm <peter.t.malcolm@gmail.com>
2017-12-29 10:31:35 -0500

    new assignment method! (untested)

--- hungarian.rb
+++ hungarian.rb
@@ -127,17 +127,91 @@ class Hungarian
 		def assign
 			r_assignments = Array.new # rows
 			c_assignments = Array.new # columns
-			@grid.each_with_index do |row, r_idx|
-				row.each_with_index do |val, v_idx|
-					if 0 === val
-						if !r_assignments.include? r_idx and !c_assignments.include? v_idx
-							r_assignments.push(r_idx)
+			# @grid.each_with_index do |row, r_idx|
+			# 	row.each_with_index do |val, v_idx|
+			# 		if 0 == val
+			# 			if !r_assignments.include? r_idx and !c_assignments.include? v_idx
+			# 				r_assignments.push(r_idx)
+			# 				c_assignments.push(v_idx)
+			# 			end
+			# 		end
+			# 	end
+			# end
+			totals = count_row_column_zeroes
+			min_row_col = find_smallest_nonzero_collection_of_zeroes(totals)
+			sanity = 0
+			sanity_max = @grid.count * 3
+			while !min_row_col[ :winner ].nil? and sanity < sanity_max
+				sanity += 1
+				if min_row_col[ :winner ].nil? # no non-zero collections left
+					return { :row => r_assignments, :column => c_assignments }
+				elsif min_row_col[ :winner ] = 'r'
+					done_marking = false
+					@grid[ min_row_col[ :row ] ].each_with_index do |val,v_idx|
+						if !done_marking and val == 0
+							r_assignments.push(min_row_col[ :row ])
 							c_assignments.push(v_idx)
+							done_marking = true
+							# decrement all corresponding column totals with zeroes in this row
+							totals[ :column ][ v_idx ] = totals[ :column ][ v_idx ] - 1
 						end
 					end
+					# set this row to zero total
+					totals[ :row ][ min_row_col[ :row ]] = 0
+				elsif min_row_col[ :winner ] = 'c'
+					done_marking = false
+					@grid.each_with_index do |row,r_idx|
+						if !done_marking and row[ min_row_col[ :column ] ] == 0
+							r_assignments.push(r_idx)
+							c_assignments.push(min_row_col[ :column ])
+							done_marking = true
+							# decrement all corresponding row totals with zeroes in this column
+							totals[ :row ][ r_idx ] = totals[ :row ][ r_idx ] - 1
+						end
+					end
+					# set this column to zero total
+					totals[ :column ][ min_row_col[ :column ]] = 0
+				end
+				min_row_col = find_smallest_nonzero_collection_of_zeroes(totals)
+			end
+			if sanity >= sanity_max
+				puts "Error! Looping on Hungarian assignment step failed with totals: \n" + totals.inspect
+				exit
+			end
+		end
+
+		def count_row_column_zeroes
+			r_total = Array.new
+			c_total = Array.new
+			@grid.each do |row|
+				r_total.push(row.count(0))
+			end
+			@grid.transpose.each do |column|
+				c_total.push(column.count(0))
+			end
+			return { :row => r_total, :column => c_total }
+		end
+
+		def find_smallest_nonzero_collection_of_zeroes(totals)
+			min = @grid.count
+			rows_v_cols = nil
+			which_row = nil
+			which_col = nil
+			for totals[ :row ].each_with_index do |row_tot,r_idx|
+				if row_tot < min and row_tot > 0
+					min = row_tot
+					which_row = r_idx
+					rows_v_cols = 'r'
 				end
 			end
-			return { :row => r_assignments, :column => c_assignments }
+			for totals[ :column ].each_with_index do |col_tot,c_idx|
+				if col_tot < min and col_tot > 0
+					min = col_tot
+					which_col = c_idx
+					rows_v_cols = 'c'
+				end
+			end
+			return { :winner => rows_v_cols, :row => which_row, :column => which_col }
 		end
 
 		def mark
@@ -152,7 +226,7 @@ class Hungarian
 					unassigned_or_duplicate_rows.push(u_row)
 				end
 				@grid[u_row].each_with_index do |val, v_idx|
-					if 0 === val and !capturing_columns.include? v_idx
+					if 0 == val and !capturing_columns.include? v_idx
 						capturing_columns.push(v_idx) # capturing cols get zeroes from un_rows
 						@grid.each_with_index do |row, r_idx|
 							if assigned?(r_idx,v_idx) and !unassigned_or_duplicate_rows.include? r_idx
